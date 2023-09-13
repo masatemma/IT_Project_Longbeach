77565a06de351f47954218c20576282e4ce8eedb
Nathan Vu <106425578+Mesopeer365@users.noreply.github.com>
2023-09-13 11:26:41 +1000

    changed link to access database

--- mern/server/routes/record.mjs
+++ mern/server/routes/record.mjs
@@ -5,14 +5,14 @@ import { ObjectId } from "mongodb";
 const router = express.Router();
 
 // This section will help you get a list of all the records.
-router.get("/", async (req, res) => {
+router.get("/attendee/", async (req, res) => {
   let collection = await db.collection("Attendee");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
 
 // This section will help you get a single record by id
-router.get("/:id", async (req, res) => {
+router.get("/attendee/:id", async (req, res) => {
   let collection = await db.collection("Attendee");
   let query = {_id: new ObjectId(req.params.id)};
   let result = await collection.findOne(query);
@@ -21,8 +21,23 @@ router.get("/:id", async (req, res) => {
   else res.send(result).status(200);
 });
 
+router.get("/classes", async (req, res) => {
+  let collection = await db.collection("Class");
+  let results = await collection.find({}).toArray();
+  res.send(results).status(200);
+});
+
+router.get("/classes/:id", async (req, res) => {
+  let collection = await db.collection("Session");
+  let query = {_id: new ObjectId(req.params.class_id)};
+  let result = await collection.findOne(query);
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
 // Create a new record.
-router.post("/", async (req, res) => {
+router.post("/attendee/", async (req, res) => {
   let newDocument = {
     first_name: req.body.first_name,  
     last_name: req.body.last_name,    
@@ -34,7 +49,7 @@ router.post("/", async (req, res) => {
 });
 
 // This section will help you update a record by id.
-router.patch("/:id", async (req, res) => {
+router.patch("/attendee/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
   const updates =  {
     $set: {
@@ -51,7 +66,7 @@ router.patch("/:id", async (req, res) => {
 });
 
 // This section will help you delete a record
-router.delete("/:id", async (req, res) => {
+router.delete("/attendee/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
 
   const collection = db.collection("Attendee");
